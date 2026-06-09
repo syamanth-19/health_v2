ad39d8d1d5adf184e1f25bd5ed40e3104c478da3
syamanth-19 <syamanthjogi66@gmail.com>
2026-06-09 12:27:05 +0530

    first commit

---
+++ health_app/health/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <p>Hello ! Welcome to health app </p>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+p {
+  text-align: center;
+  font-weight: bold;
+  
+}
+</style>
